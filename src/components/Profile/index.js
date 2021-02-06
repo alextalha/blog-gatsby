264d00570ce6f6c145cb534f7795ad1b19db14aa
Alex Talha <alex.talha@original.io>
2021-02-06 16:56:12 -0300

    + alguns ajuste no template

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import  React from "react"
 import {graphql, useStaticQuery } from "gatsby"
+import Avatar from "../Avatar"
 
 
 const Profile = () => {
@@ -24,6 +25,7 @@ const Profile = () => {
 
     return (
         <div className="Profile-wrapper">
+            <Avatar />
             <h1>{title}</h1>
             <h2>{position}</h2>
             <p>{description}</p>
